d7b537e95b1a7ecde4a64367ae529561bb1f71bd
ruccho <ruccho@ruccho.com>
2026-02-17 16:26:52 +0900

    fix ffmpeg encoding system type

--- InstantReplay.Externals/unienc/crates/unienc/src/platform.rs
+++ InstantReplay.Externals/unienc/crates/unienc/src/platform.rs
@@ -12,7 +12,7 @@ pub type PlatformEncodingSystem<V, A, R> = unienc_windows_mf::MediaFoundationEnc
 pub type PlatformEncodingSystem<V, A, R> = unienc_webcodecs::WebCodecsEncodingSystem<V, A, R>;
 
 #[cfg(all(unix, not(any(target_vendor = "apple", target_os = "android", windows, target_arch = "wasm32"))))]
-pub type PlatformEncodingSystem<V, A, R> = unienc_ffmpeg::FFmpegEncodingSystem<V, A>;
+pub type PlatformEncodingSystem<V, A, R> = unienc_ffmpeg::FFmpegEncodingSystem<V, A, R>;
 
 #[cfg(not(any(target_vendor = "apple", target_os = "android", windows, unix, target_arch = "wasm32")))]
 pub type PlatformEncodingSystem<V, A, R> = ();
