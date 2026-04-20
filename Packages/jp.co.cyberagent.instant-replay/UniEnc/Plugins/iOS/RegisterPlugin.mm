959175f1b6c9366854d8b103b6ca46ad09072688
ruccho <ruccho@ruccho.com>
2026-04-20 11:59:48 +0900

    fix AppController conflict

--- Packages/jp.co.cyberagent.instant-replay/UniEnc/Plugins/iOS/RegisterPlugin.mm
+++ Packages/jp.co.cyberagent.instant-replay/UniEnc/Plugins/iOS/RegisterPlugin.mm
@@ -1,19 +1,24 @@
 #import "UnityAppController.h"
+#import "PluginBase/LifeCycleListener.h"
 #include "Unity/IUnityGraphics.h"
 
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API unienc_UnityPluginLoad(IUnityInterfaces* unityInterfaces);
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API unienc_UnityPluginUnload();
 
-@interface MyAppController : UnityAppController
-{
-}
-- (void)shouldAttachRenderDelegate;
+@interface UniEncLifeCycleListener : NSObject<LifeCycleListener>
 @end
-@implementation MyAppController
-- (void)shouldAttachRenderDelegate
-{
-    UnityRegisterRenderingPluginV5(&unienc_UnityPluginLoad, &unienc_UnityPluginUnload);
+
+@implementation UniEncLifeCycleListener
+
++ (void)load {
+    static UniEncLifeCycleListener* listener = nil;
+    listener = [[self alloc] init];
+    UnityRegisterLifeCycleListener(listener);
+}
+
+- (void)didFinishLaunching:(NSNotification*)notification {
+    UnityRegisterRenderingPluginV5(&unienc_UnityPluginLoad,
+                                   &unienc_UnityPluginUnload);
 }
 
 @end
-IMPL_APP_CONTROLLER_SUBCLASS(MyAppController);
